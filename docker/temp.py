8f7c5b5cb8cb9a30d26016e9a7fe8b02d801c15e
Sohaib  Waheed <sohaib@wonder-tech.co>
2023-12-26 12:23:18 +0500

    init

---
+++ docker/temp.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+
+image = plt.imread(r'C:\Users\SohaibWaheed\OneDrive - WonderTree\Projects\Virtual_Try_On\temp_train\cloth\a_man1.jpg')
+plt.imshow(image)
+plt.show()
